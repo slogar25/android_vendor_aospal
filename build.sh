eac1291b2e1beed750929883af470d39915374ca
xilence2210 <xilence2210@gmail.com>
2013-12-14 15:01:14 +0800

    build.sh: implement in my way
     - simply use it with no parameters will delete build.prop for
    clean build
     - add 'fix','clean','allclean','sync' for not delete build.prop,
    mka installclean,mka clean and repo sync before build
    
    Change-Id: I10e3b501afa6c0e298b9296422c1a43eff69f22d

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-# Version 2.0.4, Adapted for AOSPA.
+# Version 2.0.4, Adapted for AOSPAL.
+
+if [ "pa$1" == "pa" ]
+then
+   echo "Usage: './mk [Device] {Variable}'"
+   echo "   Device - your device name"
+   echo "   Variable - functions"
+   echo "      fix   :start build without any cleanning for fix build"
+   echo "      clean :run 'make installclean' before build"
+   echo "      sync  :run 'repo sync' before build"
+   echo " "
+   exit 0
+fi
 
 # We don't allow scrollback buffer
 echo -e '\0033\0143'
@@ -36,8 +48,8 @@ fi
 JVER=$(javac -version  2>&1 | head -n1 | cut -f2 -d' ')
 
 # Import command line parameters
+THREADS="4"
 DEVICE="$1"
-EXTRAS="$2"
 
 # Get build version
 MAJOR=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAJOR := *' | sed  's/ROM_VERSION_MAJOR := //g')
@@ -46,14 +58,6 @@ MAINTENANCE=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAINTENANCE := *'
 
 VERSION=$MAJOR.$MINOR$MAINTENANCE
 
-# If there is no extra parameter, reduce parameters index by 1
-if [ "$EXTRAS" == "true" ] || [ "$EXTRAS" == "false" ]; then
-        SYNC="$2"
-        UPLOAD="$3"
-else
-        SYNC="$3"
-        UPLOAD="$4"
-fi
 
 # Get start time
 res1=$(date +%s.%N)
@@ -61,35 +65,12 @@ res1=$(date +%s.%N)
 echo -e "${cya}Building ${bldcya}AOSPA $VERSION for $DEVICE ${txtrst}";
 echo -e "${bldgrn}Start time: $(date) ${txtrst}"
 
-# Decide what command to execute
-case "$EXTRAS" in
-        threads)
-                echo -e "${bldblu}Please enter desired building/syncing threads number followed by [ENTER]${txtrst}"
-                read threads
-                THREADS=$threads
-        ;;
-        clean|cclean)
-                echo -e "${bldblu}Cleaning intermediates and output files${txtrst}"
-                export CLEAN_BUILD="true"
-                [ -d "${DIR}/out" ] && rm -Rf ${DIR}/out/*
-        ;;
-esac
-
-echo -e ""
-
-# Fetch latest sources
-if [ "$SYNC" == "true" ]; then
-        echo -e ""
-        echo -e "${bldblu}Fetching latest sources${txtrst}"
-        repo sync -j"$THREADS"
-        echo -e ""
-fi
-
 if [ ! -r "${DIR}/out/versions_checked.mk" ] && [ -n "$(java -version 2>&1 | grep -i openjdk)" ]; then
         echo -e "${bldcya}Your java version still not checked and is candidate to fail, masquerading.${txtrst}"
         JAVA_VERSION="java_version=${JVER}"
 fi
 
+makeme=0
 if [ -n "${INTERACTIVE}" ]; then
         echo -e "${bldblu}Dropping to interactive shell${txtrst}"
         echo -en "${bldblu}Remeber to lunch you device:"
@@ -103,6 +84,10 @@ else
         # Setup environment
         echo -e ""
         echo -e "${bldblu}Setting up environment${txtrst}"
+        export USE_CCACHE=1
+        export CCACHE_DIR=~/.ccache
+        # set ccache due to your disk space,set it at your own risk
+        prebuilts/misc/linux-x86/ccache/ccache -M 15G
         . build/envsetup.sh
         echo -e ""
 
@@ -110,7 +95,44 @@ else
         echo -e "${bldblu}Lunching device [$DEVICE] ${cya}(Includes dependencies sync)${txtrst}"
         export PREFS_FROM_SOURCE
         lunch "pa_$DEVICE-userdebug";
+        
+        makeme=1
+fi
 
+# Decide what command to execute
+for var in $* ; do
+if [ "$var" == "sync" ]
+then
+   echo -e "${bldblu}Fetching latest sources ${txtrst}"
+   repo sync
+   echo -e ""
+elif [ "$var" == "clean" ]
+then
+   echo -e "${bldblu}Clearing previous build info ${txtrst}"
+   mka installclean
+elif [ "$var" == "allclean" ]
+then
+   echo -e "${bldblu}Clearing build path ${txtrst}"
+   mka clean
+elif [ "$var" == "fix" ]
+then
+   echo -e "skip for remove build.prop"
+   fix_count=1
+else
+   echo -e "running..."
+fi
+done
+
+if [ "$fix_count" == "0" ]
+then
+   echo -e "removing build.prop"
+   rm -f $DIR/out/target/product/$DEVICE/system/build.prop
+fi
+
+echo -e ""
+
+if [ "$makeme" == "1" ]
+then
         echo -e "${bldblu}Starting compilation${txtrst}"
         mka bacon
 fi
