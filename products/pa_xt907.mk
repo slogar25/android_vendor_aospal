9087bf17e9b639379e13754cc37b75791041636a
Andrew <ayunker551@gmail.com>
2014-01-29 16:38:17 -0500

    not sure why this was changed

--- products/pa_xt907.mk
+++ products/pa_xt907.mk
@@ -17,7 +17,7 @@
 ifeq (pa_xt907,$(TARGET_PRODUCT))
 
 # OVERLAY_TARGET adds overlay asset source
-OVERLAY_TARGET := pa_xhdpi
+OVERLAY_TARGET := pa_hdpi
 
 # Build paprefs from sources
 PREFS_FROM_SOURCE ?= false
